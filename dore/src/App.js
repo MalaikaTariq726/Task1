7aacdf5aa2b79aad8f305eb62e1d2045769367e6
Malaika Tariq <tariqmalaika09@gmail.com>
2024-07-22 12:03:38 +0500

    with theme

--- dore/src/App.js
+++ dore/src/App.js
@@ -1,22 +1,11 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
-import DashboardDark from './pages/dasboarddark';
-import UserTable from './components/Table';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/dashboarddark" element={<DashboardDark />} />
-        
-        <Route path="/table" element={<UserTable />} />
-
-      </Routes>
-      </Router>
+    <Dashboard/>
     </div>
   );
 }
